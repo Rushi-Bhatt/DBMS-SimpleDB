fd6eefac479e308ab5d36171ef58208bb2912c84
Rushi Bhatt <rbhatt@ncsu.edu>
2017-05-01 23:45:48 -0400

    Update basicBufferMgr.java

--- simpleDB/simpledb/buffer/BasicBufferMgr.java
+++ simpleDB/simpledb/buffer/BasicBufferMgr.java
@@ -18,11 +18,8 @@ import simpledb.file.FileMgr;
 public class BasicBufferMgr {
 	public Buffer[] bufferpool;
 	private int numAvailable;
-	// CSC-540 Buffer Management Map to store block buffer mapping(Task1)
 	public HashMap<Block, Buffer> bufferPoolMap;
-	// CSC-540 Buffer Management List to store the current free buffers(No block
-	// allocated yet).
-	public LinkedList<Buffer> freeBuffers;
+	public LinkedList<Buffer> availableBuf;
 
 	/**
 	 * Creates a buffer manager having the specified number of buffer slots.
@@ -39,27 +36,14 @@ public class BasicBufferMgr {
 	BasicBufferMgr(int numbuffs) {
 		bufferpool = new Buffer[numbuffs];
 		numAvailable = numbuffs;
-		// initialize hash Map
 		bufferPoolMap = new HashMap<Block, Buffer>(numbuffs);
-		// initialize free buffers.
-		freeBuffers = new LinkedList<Buffer>();
+		availableBuf = new LinkedList<Buffer>();
 		for (int i = 0; i < numbuffs; i++) {
 			bufferpool[i] = new Buffer(i);
-			freeBuffers.add(bufferpool[i]); // initialize all to be free buffers
+			availableBuf.add(bufferpool[i]);
 		}
 	}
-
-	// display the contents of all the buffers.
-	public String toString() {
-		String buffInfo = new String();
-		for (Buffer buff : bufferpool) {
-			buffInfo += buff.toString() + System.getProperty("line.separator"); // Use
-																				// system
-																				// newline
-		}
-		return buffInfo;
-	}
-
+	
 	/**
 	 * Flushes the dirty buffers modified by the specified transaction.
 	 * 
@@ -67,14 +51,12 @@ public class BasicBufferMgr {
 	 *            the transaction's id number
 	 */
 	synchronized void flushAll(int txnum) {
-		Buffer buff1 = null;
-				Iterator<Block> iterator1 = bufferPoolMap.keySet().iterator();
-		while (iterator1.hasNext()) {
-			Block bkey = iterator1.next();
-			buff1 = bufferPoolMap.get(bkey);
-				//for (Buffer buff : bufferpool)
-			if (buff1.isModifiedBy(txnum))
-				buff1.flush();
+		Buffer buff = null;
+		for (Block key : bufferPoolMap.keySet()) {
+			buff = bufferPoolMap.get(key);
+			if (buff.isModifiedBy(txnum)) {
+				buff.flush();
+			}
 		}
 	}
 
@@ -90,22 +72,19 @@ public class BasicBufferMgr {
 	 */
 	synchronized Buffer pin(Block blk) {
 		Buffer buff = findExistingBuffer(blk);
-		Block lastBlock = null;
 		if (buff == null) {
 			buff = chooseUnpinnedBuffer();
-			if (buff == null)
+			if (buff == null) {
 				return null;
-			lastBlock = buff.block();
-			// if last block not null remove it from hashMap
-			if (lastBlock != null)
-				bufferPoolMap.remove(lastBlock);
-			// Allocating a new block to the buffer
-			bufferPoolMap.put(blk, buff);
+			}
+			if (buff.block() != null) {
+				bufferPoolMap.remove(buff.block());
+			}
 			buff.assignToBlock(blk);
 		}
-		if (!buff.isPinned()) {
+		if (!buff.isPinned())
 			numAvailable--;
-		}
+		bufferPoolMap.put(blk, buff);
 		buff.pin();
 		return buff;
 	}
@@ -118,19 +97,14 @@ public class BasicBufferMgr {
 	 * @param filename
 	 *            the name of the file
 	 * @param fmtr
-	 *            a pageformatter object, used to format the new block
+	 *            a page formatter object, used to format the new block
 	 * @return the pinned buffer
 	 */
 	synchronized Buffer pinNew(String filename, PageFormatter fmtr) {
 		Buffer buff = chooseUnpinnedBuffer();
-		Block lastBlock = null;
-		if (buff == null)
-			return null;
-		lastBlock = buff.block();
-		// if last block not null remove it from hashMap
-		if (lastBlock != null)
-			bufferPoolMap.remove(lastBlock);
-		// Allocating a new block to the buffer
+		if (buff == null) {
+			throw new BufferAbortException();
+		}
 		buff.assignToNew(filename, fmtr);
 		bufferPoolMap.put(buff.block(), buff);
 		numAvailable--;
@@ -159,109 +133,86 @@ public class BasicBufferMgr {
 		return numAvailable;
 	}
 
-	// CSC-540 Buffer Management- Uses Map to determine whether block is in
-	// buffer currently
 	private Buffer findExistingBuffer(Block blk) {
-
-		Buffer buff = bufferPoolMap.get(blk);
-
-		if (buff!= null) {
-			//System.out.println("Block " + blk.number() + " already in buffer " + x); // For
-																						// Debug
-			return buff;
-
+		if (bufferPoolMap.get(blk) != null) {
+			return bufferPoolMap.get(blk);
 		} else {
 			return null;
 		}
 	}
 
-	// CSC-540 Buffer Management-Uses Least Recently Modified Buffer Replacement
-	// Policy
 	private Buffer chooseUnpinnedBuffer() {
-		//getStatistics();(Can be used to get the statistics whenever a unpinned buffer is chosen)
 
-		int maxLSN = Integer.MIN_VALUE;
-		Buffer currentBuffer = null;
+		int min = Integer.MIN_VALUE;
+		Buffer curr = null;
 		Buffer buff = null;
 
-		// If any buffer is still unallocated then allocate it
-		if (!freeBuffers.isEmpty()) {
-			buff = freeBuffers.getFirst();
-			freeBuffers.removeFirst();
-			System.out.println("New Buffer " + buff.getBufferIndex() + "Allocated");
+		if (!availableBuf.isEmpty()) {
+			buff = availableBuf.getFirst();
+			availableBuf.removeFirst();
+			System.out.println("New Buffer Allocated ------ "+buff.getBufferIndex());
 			return buff;
-		} 
-		
-		// Chooses modified page with lowest non-negative LSN
-		Iterator<Block> iterator1 = bufferPoolMap.keySet().iterator();
-		while (iterator1.hasNext()) {
-			Block bkey = iterator1.next();
-			currentBuffer = bufferPoolMap.get(bkey);
-			if (!currentBuffer.isPinned() && currentBuffer.modifiedBy >= 0
-					&& currentBuffer.logSequenceNumber > maxLSN
-					&& currentBuffer.logSequenceNumber >= 0) {
-				buff = currentBuffer;
-				maxLSN = buff.logSequenceNumber;
-
-			}
-
 		}
 
-		// If no buffer is modified we look for the first unpinned,un-modified
-		// buffer and choose it for replacement.
+		for (Block key : bufferPoolMap.keySet()) {
+			curr = bufferPoolMap.get(key);
+			if (!curr.isPinned() && curr.modifiedBy >= 0)
+				{
+					if(min < curr.logSequenceNumber && curr.logSequenceNumber >= 0)
+					{
+						buff = curr;
+						min = buff.logSequenceNumber;
+					}
+				}
+			}
+
 		if (buff == null) {
-			Iterator<Block> iterator2 = bufferPoolMap.keySet().iterator();
-			while (iterator2.hasNext()) {
-				Block bkey = iterator2.next();
-				currentBuffer = bufferPoolMap.get(bkey);
-
-				if (!currentBuffer.isPinned()) {
-					buff =currentBuffer;
-
-					System.out.println("Buffer Choosen For Replacement " + buff.getBufferIndex());
+			for (Block key : bufferPoolMap.keySet()) {
+				curr = bufferPoolMap.get(key);
+				if (!curr.isPinned()) {
+					buff = curr;
+					System.out.println("Buffer which is choosen for replacement ------- "+buff.getBufferIndex());
 					return buff;
 				}
 			}
 		}
-		if(buff!=null)
-		System.out.println("Buffer Choosen For Replacement " + buff.getBufferIndex());
+		else
+		{
+			System.out.println("Buffer which is choosen for replacement ------- "+buff.getBufferIndex());
+		}
 		return buff;
 
 	}
 
-	// Get the statistics for the buffer.
 	public void getStatistics() {
-		System.out.println("Status of the buffer pool");
-		Buffer buff1 = null;
-		//for (Buffer buff1 : bufferpool) {
-		Iterator<Block> iterator2 = bufferPoolMap.keySet().iterator();
-		while (iterator2.hasNext()) {
-			Block bkey = iterator2.next();
-			buff1 = bufferPoolMap.get(bkey);
-			int bufferReadCount = buff1.getReadCount();
-			int bufferWriteCount = buff1.getWriteCount();
-			System.out.println("Buffer " + buff1.getBufferIndex() + " LSN=" + buff1.logSequenceNumber + "Modified By"
-					+ buff1.modifiedBy + " Pin Count " + buff1.pins + " isPinned=" + buff1.isPinned() + " Read Count " + bufferReadCount + " Write Count" + bufferWriteCount);
-
-			/*int bufferReadCount = buff1.getReadCount();
-			System.out.println("The read count of buffer " + buff1.getBufferIndex() + " " + bufferReadCount);
-			int bufferWriteCount = buff1.getWriteCount();
-			System.out.println("The write count of buffer " + buff1.getBufferIndex() + " " + bufferWriteCount);*/
+		Buffer buff = null;
+		for (Block key : bufferPoolMap.keySet()) {
+			buff = bufferPoolMap.get(key);
+			System.out.println("Statistics for the Buffer ------- "+buff.getBufferIndex());
+			System.out.println("Read Count -----------------------"+buff.getReadCount());
+			System.out.println("Write Count ----------------------"+buff.getWriteCount());
+			System.out.println("Log Sequence Number --------------"+buff.logSequenceNumber);
+			System.out.println();
 		}
-		System.out.println();
+		System.out.println("-----------------------------------------------------------------");
 	}
 
-	//CSC-540 Buffer Management- Test method given in the project description
-	public boolean containsMapping(Block blk)
-	{
-		return bufferPoolMap.containsKey(blk);
-	}
-	
-	////CSC-540 Buffer Management - Test method given in the project description
-	public Buffer getMapping(Block blk)
-	{
-		return bufferPoolMap.get(blk);
-	}
-	
-	
+	/**  
+	* Determines whether the map has a mapping from  
+	* the block to some buffer.  
+	* @paramblk the block to use as a key  
+	* @return true if there is a mapping; false otherwise  
+	*/  
+	public boolean containsMapping(Block blk) {  
+		return bufferPoolMap.containsKey(blk);  
+	} 
+	/**  
+	* Returns the buffer that the map maps the specified block to.  
+	* @paramblk the block to use as a key  
+	* @return the buffer mapped to if there is a mapping; null otherwise  
+	*/  
+	public Buffer getMapping(Block blk) {  
+		return bufferPoolMap.get(blk);  
+	} 
+
 }
